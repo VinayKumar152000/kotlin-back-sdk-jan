af793b1b8c86ea2f3dbf4579e74bfb7f37790ba4
VinayKumar152000 <geekmosh15@gmail.com>
2023-06-21 22:46:04 +0530

    adding first commit

---
+++ src/test/java/com/example/kotlinbacksdk/KotlinBackSdkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kotlinbacksdk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KotlinBackSdkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
